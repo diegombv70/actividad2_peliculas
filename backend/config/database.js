fa7ee4493f7088cb1c16d6d5a56f2f15466625a5
Diego Bedoya <diego.bedoya@confiar.coop>
2025-04-02 14:24:22 -0500

    Final

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,31 @@
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "C:/Users/diego.bedoya/Documents/Iudigital/IngenieriaWeb2/Actividad2/appeliculas.db",
-  logging: true, // Desactiva logs en consola
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "C:/Users/diego.bedoya/Documents/Iudigital/IngenieriaWeb2/Actividad2/appeliculas.db",
+//   logging: true, // Desactiva logs en consola
+// });
+
+// module.exports = sequelize;
+
+
+// sequelize.sync({ alter: true }) // O { force: true } si quieres recrear la BD
+//     .then(() => console.log("📦 Base de datos sincronizada"))
+//     .catch(err => console.error("❌ Error al sincronizar la base de datos:", err));
+
+
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // Necesario en Render
+    }
 });
 
-module.exports = sequelize;
+pool.connect()
+    .then(() => console.log('✅ Conectado a PostgreSQL'))
+    .catch(err => console.error('❌ Error de conexión:', err));
 
-
-sequelize.sync({ alter: true }) // O { force: true } si quieres recrear la BD
-    .then(() => console.log("📦 Base de datos sincronizada"))
-    .catch(err => console.error("❌ Error al sincronizar la base de datos:", err));
+module.exports = pool;
